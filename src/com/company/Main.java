eb12f612f4ba121f64563c6d851e7f89d8b95856
yoviq10 <quisbert.yovanna@gmail.com>
2021-06-13 12:23:35 -0400

    Solar System basic structure is ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SolarSystem FactorX =new SolarSystem();
+        FactorX.Sun=1;
+        FactorX.Moon=3;
+        FactorX.Planets=2;
+
     }
 }
--- src/com/company/SolarSystem.java
+++ src/com/company/SolarSystem.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class SolarSystem {
-    
+
      int Sun;
      int Moon;
      int Planets;
